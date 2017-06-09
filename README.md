f5f7561224fd5fc2dc6075085fd7763287e4f0df
Dan D <dan.dulaney07@gmail.com>
2017-06-08 20:24:33 -0500

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# wp-aws-auto-backup
-Auto-config WordPress Backups via AWS S3
+# WP AWS Auto Config Backup Script
+Auto-config WordPress Backups via AWS S3. This is tested on Debian 8 x64 and Ubuntu 16.04 and later
+
+**Please verify that the backups are being created! This is BETA software.**
+
+## Before running, you must:
+
+* Have already set up a WordPress install
+* Have an AWS Account
+* Set up an S3 bucket
+* Create a programatic user with IAM with full S3 Access
+* Have the id and secret key for the user in the last step
+* (See below if you are unsure how to do any of the above)
+
+## Installation
+
+    sudo apt-get install git -y
+    cd ~
+    git clone https://github.com/duplaja/wp-aws-auto-backup
+    cd wp-aws-auto-backup
+    nano setup-aws.sh
+    <enter correct values for the 6 variables at the top (may not need to change SITE_DIR)>
+    <Variables are: AWS_ID, AWS_SECRET, AWS_REGION, DOMAIN, SITE_DIR, S3_BUCKET_AND_SUBFOLDER>
+    <ctrl + o to write out and save changes>
+    sudo bash setup-aws.sh
+
+## Useful Links
+
+
+* [Creating a New AWS Bucket](http://docs.aws.amazon.com/AmazonS3/latest/UG/CreatingaBucket.html)
+* [Creating a New AWS User (Programatic)](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html#id_users_create_console)
+
+## Suggestions and Ideas
+
+* Look into setting up a S3 bucket lifecycle to auto remove files older than x days
+* You can also choose to move older backups into long term storage via Glacier
+* Although costs are low, don't forget that you're paying for AWS! It's about $0.03 / GB in most US regions, so if you don't version, you will eventually build up some significant costs.
+* With 30 day versioning, a 1gb site can expect to pay $0.90 / month for backups.
+* You can always run your script manually, by navigating to /var/other-scripts and using bash backup-script.sh
---
+++ setup-aws.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+##############################################
+# 
+# Variables: Please make sure all are 
+# appropriately filled out before running 
+#
+###############################################
+
+# AWS ID and Secret Should have S3 write permissions at a minimum
+# AWS Region should match your bucket region
+AWS_ID="ID HERE"
+AWS_SECRET="SECRET HERE"
+AWS_REGION="us-west-2"
+
+# Your domain without the http/https, absolute site file location and S3 Bucket information
+DOMAIN="DOMAINHERE"
+SITE_DIR="/var/www/$DOMAIN"
+S3_BUCKET_AND_SUBFOLDER="bucket/subfolder"
+
+
+#Pulls the DB Name, User, and PW from wp-config.php of the site located in SITE_DIR
+WP_CONFIG_FILE="${SITE_DIR}/wp-config.php"
+DB_NAME=$(cat ${WP_CONFIG_FILE} | grep DB_NAME | cut -d \' -f 4)
+DB_USER=$(cat ${WP_CONFIG_FILE} | grep DB_USER | cut -d \' -f 4)
+DB_PASSWORD=$(cat ${WP_CONFIG_FILE} | grep DB_PASSWORD | cut -d \' -f 4)
+
+# Installs AWS CLI
+apt-get --assume-yes install awscli
+
+# Creates a profile for default using your AWS S3 credentials from above
+mkdir ~/.aws
+echo '[default]' > ~/.aws/config
+echo region=$AWS_REGION >> ~/.aws/config
+echo '[default]' > ~/.aws/credentials
+echo aws_access_key_id=$AWS_ID >> ~/.aws/credentials
+echo aws_secret_access_key=$AWS_SECRET >> ~/.aws/credentials
+
+echo "AWS Configured..."
+# Create your customized backup script folder
+mkdir /var/other-scripts
+
+# Copy the two partial templates to the folder above
+cp backup-site.sh /var/other-scripts/backup-site.sh
+cp backuptemplate /var/other-scripts/backuptemplate
+
+# Start filling in the lines for the user-entered variables above
+echo "DOMAIN=\"$DOMAIN\"" >> /var/other-scripts/backup-site.sh
+echo "SITE_DIR=\"$SITE_DIR\"" >> /var/other-scripts/backup-site.sh
+echo "S3_BUCKET_AND_SUBFOLDER=\"$S3_BUCKET_AND_SUBFOLDER\"" >> /var/other-scripts/backup-site.sh
+
+# Copy over database credentials
+echo "DB_NAME=\"$DB_NAME\"" >> /var/other-scripts/backup-site.sh
+echo "DB_USER=\"$DB_USER\"" >> /var/other-scripts/backup-site.sh
+echo "DB_PASSWORD=\"$DB_PASSWORD\"" >> /var/other-scripts/backup-site.sh
+
+
+# Reads the bulk of the script from the template file into the shell script
+cat /var/other-scripts/backuptemplate >> /var/other-scripts/backup-site.sh
+
+# Cleans up the partial template file now that it's no longer needed
+rm /var/other-scripts/backuptemplate
+
+# Marks the backup script as executable
+chmod +x /var/other-scripts/backup-site.sh
+
+echo "Backup Script Created and Configured at /var/other-scripts/backup-site.sh"
+
+# Adds the backup to crontab nightly at 1:05 am
+(crontab -l ; echo "05 01 * * * bash /var/other-scripts/backup-site.sh") | crontab -
+
+echo "Backup Added to Nightly Cron at 1:05 am server time"
